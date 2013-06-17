f153fa6fc902b51936a567c1ab69e2cb22262abc
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-17 14:33:07 +0200

    initial

---
+++ Quadtree.hs
@@ -0,0 +1,58 @@
+module Quadtree
+where
+
+import Data.Bits
+
+type Vec2 = (Int, Int)
+
+data Quadtree a = Quadtree Int (Quad a)
+                deriving (Show)
+
+data Quad a = Node { nw, ne, sw, se :: Quad a }
+            | Leaf a
+            | Empty
+            deriving (Show)
+
+data QDir   = NW | NE | SW | SE
+            deriving (Eq, Ord, Enum, Show, Bounded)
+
+quadStep :: Quad a -> QDir -> Quad a
+quadStep qt NW = nw qt
+quadStep qt NE = ne qt
+quadStep qt SW = se qt
+quadStep qt SE = sw qt
+
+pathTo :: (Int, Int) -> Int -> [QDir]
+pathTo _     0 = []
+pathTo pt@(x,y) k =
+    case p of
+        (False, False) -> NW:pathTo pt nk
+        (True , False) -> NE:pathTo pt nk
+        (False, True ) -> SW:pathTo pt nk
+        (True , True ) -> SE:pathTo pt nk
+    where   nk = k - 1
+            x' = testBit x nk
+            y' = testBit y nk
+            p  = (x',y')
+
+applyByPath :: (Quad a -> Quad a) -> [QDir] -> Quad a -> Quad a
+applyByPath f []          qt    = f qt
+applyByPath f p           Empty = applyByPath f p (Node Empty Empty Empty Empty)
+applyByPath f (step:path) node  = collapse node'
+    where
+        collapse n =
+            case n of
+                (Node Empty Empty Empty Empty)  -> Empty
+                _                               -> n
+        node' =
+            case step of
+                NW -> node{ nw = applyByPath f path (nw node) }
+                NE -> node{ ne = applyByPath f path (ne node) }
+                SW -> node{ sw = applyByPath f path (sw node) }
+                SE -> node{ se = applyByPath f path (se node) }
+
+insert :: Vec2 -> a -> Quadtree a -> Quadtree a
+insert pt val (Quadtree k q) =
+    Quadtree k (applyByPath insert' path q)
+    where   path      = pathTo pt k
+            insert' _ = Leaf val
