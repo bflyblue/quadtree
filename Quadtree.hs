65387bb12810ae5ea7d9ee028d67dc62df64a042
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-18 17:06:43 +0200

    playing

--- Quadtree.hs
+++ Quadtree.hs
@@ -42,7 +42,26 @@ top q = Zipper q []
 adjust :: QTrav a -> Zipper a -> Zipper a
 adjust f (Zipper q bs) = Zipper (f q) bs
 
-up :: Eq a => Zipper a -> Maybe (Zipper a)
+emptynode :: Int -> Quad a
+emptynode k = Node k e e e e
+    where e = Empty (k - 1)
+
+isEmpty :: Quad a -> Bool
+isEmpty Empty{} = True
+isEmpty _       = False
+
+emptyexpand :: Quad a -> Quad a
+emptyexpand Empty{level=k} = emptynode k
+emptyexpand q              = q
+
+emptycollapse :: Quad a -> Quad a
+emptycollapse n@(Node k a b c d) =
+    if all isEmpty [a,b,c,d]
+    then Empty{level=k}
+    else n
+emptycollapse n = n
+
+up :: Zipper a -> Maybe (Zipper a)
 up zipper =
     case zipper of
         Zipper _  []                            -> Nothing
@@ -51,21 +70,7 @@ up zipper =
         Zipper sw' (SWCrumb k nw' ne' se' : bs) -> Just $ Zipper (n k nw' ne' sw' se') bs
         Zipper se' (SECrumb k nw' ne' sw' : bs) -> Just $ Zipper (n k nw' ne' sw' se') bs
     where
-        n k a b c d = emptycollapse k (Node k a b c d)
-
-emptynode :: Int -> Quad a
-emptynode k = Node k e e e e
-    where e = Empty (k - 1)
-
-emptyexpand :: Quad a -> Quad a
-emptyexpand Empty{level=k} = emptynode k
-emptyexpand q              = q
-
-emptycollapse :: Eq a => Int -> Quad a -> Quad a
-emptycollapse k q =
-    if q == emptynode k
-    then Empty{level=k}
-    else q
+        n k a b c d = emptycollapse (Node k a b c d)
 
 dn :: QTrav a -> CrumbCons a -> QTrav a -> QTrav a -> QTrav a -> Zipper a -> Maybe (Zipper a)
 dn q b b1 b2 b3 (Zipper node bs) =
@@ -84,7 +89,7 @@ ne = dn _ne NECrumb _nw _sw _se
 sw = dn _sw SWCrumb _nw _ne _se
 se = dn _se SECrumb _nw _ne _sw
 
-topmost :: Eq a => Zipper a -> Zipper a
+topmost :: Zipper a -> Zipper a
 topmost zipper =
     case up zipper of
         Just z' -> topmost z'
@@ -106,7 +111,7 @@ pathTo pt@(x,y) k z =
 empty :: Int -> Quad a
 empty = Empty
 
-modify :: Eq a => Vec2 -> (Quad a -> Quad a) -> Quad a -> Quad a
+modify :: Vec2 -> (Quad a -> Quad a) -> Quad a -> Quad a
 modify pt f q =
     case path (top q) of
         Just q' -> (quad . topmost . adjust f) q'
@@ -114,14 +119,14 @@ modify pt f q =
     where   k = depth q
             path = pathTo pt k
 
-insert :: Eq a => Vec2 -> a -> Quad a -> Quad a
+insert :: Vec2 -> a -> Quad a -> Quad a
 insert pt val = modify pt insert'
     where   insert' _ = Leaf val
 
-delete :: Eq a => Vec2 -> Quad a -> Quad a
+delete :: Vec2 -> Quad a -> Quad a
 delete pt q = modify pt delete' q
     where   k = depth q
             delete' _ = Empty k
 
-insertList :: Eq a => Quad a -> [(Vec2, a)] -> Quad a
+insertList :: Quad a -> [(Vec2, a)] -> Quad a
 insertList = foldl (\acc (pt,val) -> insert pt val acc)
