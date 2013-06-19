554fe8ac3207a7b8e28b4fc8deb16bddb2ca8247
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-19 09:41:57 +0200

    playing

--- Quadtree.hs
+++ Quadtree.hs
@@ -2,6 +2,7 @@ module Quadtree
 where
 
 import Data.Bits
+import Data.List
 
 type Vec2 = (Int, Int)
 
@@ -26,6 +27,28 @@ data Zipper a = Zipper  { quad          :: Quad a
                         , height        :: Int
                         , breadcrumbs   :: [Crumb a]
                         } deriving (Eq, Show)
+
+data Direction = NW | NE | SW | SE deriving (Eq, Ord, Bounded, Enum, Show)
+
+pathForPos :: Int -> Vec2 -> [Direction]
+pathForPos 0 _ = []
+pathForPos h p = step h p:pathForPos (h-1) p
+
+step :: Int -> Vec2 -> Direction
+step h (x,y) =
+    let h'       = h - 1
+        (xb, yb) = (testBit x h', testBit y h')
+    in  case (xb,yb) of
+            (False, False) -> NW
+            (True , False) -> NE
+            (False, True ) -> SW
+            (True , True ) -> SE
+
+commonPrefix :: Eq a => [[a]] -> [a]
+commonPrefix = map head . takeWhile (\(x:xs) -> all (==x) xs) . transpose
+
+dropCommonPrefix :: Eq a => [[a]] -> [[a]]
+dropCommonPrefix lists = map (drop (length $ commonPrefix lists)) lists
 
 up :: Zipper a -> Maybe (Zipper a)
 up (Zipper _ _     _ []    ) = Nothing
@@ -54,20 +77,16 @@ dn qsel mkcrumb (xb,yb) (Zipper q (x,y) h bc) =
             h'  = h - 1
             b'  = mkcrumb eq:bc
 
-nw, ne, sw, se :: Zipper a -> Maybe (Zipper a)
-nw = dn _nw nwCrumb (0,0)
-ne = dn _ne neCrumb (1,0)
-sw = dn _sw swCrumb (0,1)
-se = dn _se seCrumb (1,1)
-
-makeCrumb :: (Quad a -> Quad a -> Quad a -> Crumb a) -> (Quad a -> Quad a) -> (Quad a -> Quad a) -> (Quad a -> Quad a) -> Quad a -> Crumb a
+makeCrumb :: (Quad a -> Quad a -> Quad a -> Crumb a) ->
+             (Quad a -> Quad a) -> (Quad a -> Quad a) -> (Quad a -> Quad a) ->
+             Quad a -> Crumb a
 makeCrumb c b1 b2 b3 q = c (b1 q) (b2 q) (b3 q)
 
-nwCrumb, neCrumb, swCrumb, seCrumb :: Quad a -> Crumb a
-nwCrumb = makeCrumb NWCrumb _ne _sw _se
-neCrumb = makeCrumb NECrumb _nw _sw _se
-swCrumb = makeCrumb SWCrumb _nw _ne _se
-seCrumb = makeCrumb SECrumb _nw _ne _sw
+nw, ne, sw, se :: Zipper a -> Maybe (Zipper a)
+nw = dn _nw (makeCrumb NWCrumb _ne _sw _se) (0,0)
+ne = dn _ne (makeCrumb NECrumb _nw _sw _se) (1,0)
+sw = dn _sw (makeCrumb SWCrumb _nw _ne _se) (0,1)
+se = dn _se (makeCrumb SECrumb _nw _ne _sw) (1,1)
 
 top :: Zipper a -> Maybe (Zipper a)
 top zipper =
