0858928814fba9c90b8155987b89605e00292469
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-21 23:17:03 +0200

    playing

--- Quadtree2.hs
+++ Quadtree2.hs
@@ -53,60 +53,64 @@ go d  (Zipper q     h bc) =
                               in  Just $ Zipper q' (h - 1) (b':bc)
         _                  -> Nothing
 
-walk :: Ord a => Zipper a -> [Direction] -> Maybe (Zipper a)
-walk = foldM (flip go)
+-- walk :: Ord a => Zipper a -> [Direction] -> Maybe (Zipper a)
+-- walk = foldM (flip go)
 
-step :: (Bool, Bool) -> Direction
-step (False, False) = NW
-step (True , False) = NE
-step (False, True ) = SW
-step (True , True ) = SE
+step :: (Int, Int) -> Direction
+step (0,0) = NW
+step (1,0) = NE
+step (0,1) = SW
+step (1,1) = SE
 
-pathForPos :: Int -> Vec2 -> [Direction]
-pathForPos 0 _     = []
-pathForPos h (x,y) = step (xb,yb):pathForPos h' (x,y)
-    where   h' = h - 1
-            xb = testBit x h'
-            yb = testBit y h'
+bits :: Vec2 -> [(Int, Int)]
+bits (0,0) = []
+bits (x,y) = (lb x, lb y):bits (sr x, sr y)
+    where   sr i = shiftR i 1
+            lb j = j .&. 1
 
-pathFromZipper :: Zipper a -> [Direction]
-pathFromZipper (Zipper _ _ bc) =
-    map dir bc
+type Path = (Int, [Direction])
+
+pathForPos :: Vec2 -> Path
+--pathForPos = foldl (\(h,d) s -> (h+1,s:d)) (0,[]) . map step . bits
+pathForPos = (\x -> (length x, x)) . map step . reverse . bits
+
+pathFromZipper :: Zipper a -> Path
+pathFromZipper (Zipper _ h bc) = (h, map dir bc)
     where   dir (Crumb d _ _ _) = d
 
 pathTo :: [Direction] -> [Direction] -> [Direction]
 pathTo []     ds     = ds
 pathTo ss     []     = map (const UP) ss
-pathTo (s:ss) (d:ds) | s == d   = pathTo ss ds
-                     |otherwise = pathTo (s:ss) [] ++ pathTo [] (d:ds)
+pathTo (s:ss) (d:ds) | s == d    = pathTo ss ds
+                     | otherwise = pathTo (s:ss) [] ++ pathTo [] (d:ds)
 
-path :: Ord a => [Direction] -> Zipper a -> Maybe (Zipper a)
-path dest z = walk z $ pathTo (pathFromZipper z) dest
+-- path :: Ord a => [Direction] -> Zipper a -> Maybe (Zipper a)
+-- path dest z = walk z $ pathTo (pathFromZipper z) dest
 
-top :: Ord a => Zipper a -> Maybe (Zipper a)
-top = path []
+-- top :: Ord a => Zipper a -> Maybe (Zipper a)
+-- top = path []
 
-to :: Ord a => Vec2 -> Zipper a -> Maybe (Zipper a)
-to p z@(Zipper _ h bc) = path dest z
-    where dest = pathForPos (h + length bc) p
+-- to :: Ord a => Vec2 -> Zipper a -> Maybe (Zipper a)
+-- to p z@(Zipper _ h bc) = path dest z
+--     where dest = pathForPos (h + length bc) p
 
-view :: Zipper a -> Maybe (Quad a)
-view (Zipper q h _) =
-    case h of
-        0   -> Just q
-        _   -> Nothing
+-- view :: Zipper a -> Maybe (Quad a)
+-- view (Zipper q h _) =
+--     case h of
+--         0   -> Just q
+--         _   -> Nothing
 
-set :: Quad a -> Zipper a -> Maybe (Zipper a)
-set v (Zipper _ h bc) =
-    case h of
-        0   -> Just (Zipper v h bc)
-        _   -> Nothing
+-- set :: Quad a -> Zipper a -> Maybe (Zipper a)
+-- set v (Zipper _ h bc) =
+--     case h of
+--         0   -> Just (Zipper v h bc)
+--         _   -> Nothing
 
-modify :: (Quad a -> Quad a) -> Zipper a -> Maybe (Zipper a)
-modify f (Zipper q h bc) =
-    case h of
-        0   -> Just (Zipper (f q) h bc)
-        _   -> Nothing
+-- modify :: (Quad a -> Quad a) -> Zipper a -> Maybe (Zipper a)
+-- modify f (Zipper q h bc) =
+--     case h of
+--         0   -> Just (Zipper (f q) h bc)
+--         _   -> Nothing
 
-zipper :: Zipper a
-zipper = Zipper Empty 0 []
+-- zipper :: Zipper a
+-- zipper = Zipper Empty 0 []
