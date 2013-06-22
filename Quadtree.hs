a696b95e0ecc54e1a445dafaeffc2480b28eb786
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-23 00:29:05 +0200

    playing

--- Quadtree.hs
+++ Quadtree.hs
@@ -3,7 +3,7 @@ where
 
 import Data.Bits
 import Data.Word
-import Control.Parallel
+-- import Data.List
 
 data Quadtree a = Quadtree !Int !(Quad a)
                 deriving (Eq, Show)
@@ -37,40 +37,13 @@ findQuad _       n                  = n
 modifyQuad :: Eq a => (Quad a -> Quad a) -> [Direction] -> Quad a -> Quad a
 modifyQuad f []     = f
 modifyQuad f (d:ds) = collapse . modify' . expand
-    where modify' (Node nw ne sw se) =
-            case d of   NW -> Node (modifyQuad f ds nw) ne sw se
-                        NE -> Node nw (modifyQuad f ds ne) sw se
-                        SW -> Node nw ne (modifyQuad f ds sw) se
-                        SE -> Node nw ne sw (modifyQuad f ds se)
+    where   modify' (Node nw ne sw se) =
+                case d of   NW -> Node (modifyQuad f ds nw) ne sw se
+                            NE -> Node nw (modifyQuad f ds ne) sw se
+                            SW -> Node nw ne (modifyQuad f ds sw) se
+                            SE -> Node nw ne sw (modifyQuad f ds se)
+            modify' _ = error "expand didn't return node"
 
-fullcoverage :: [Direction] -> [Direction] -> Bool
-fullcoverage as bs = all (== NW) as && all (== SE) bs
-
-partial :: (t -> ([Direction], [Direction]) -> Quad a -> Quad a) -> t -> ([Direction], [Direction]) -> Quad a -> Quad a
-partial f g (a:as,b:bs) (Node nw ne sw se) =
-    case (a,b) of
-        (NW,NW) -> Node (f' nw) ne sw se
-        (NE,NE) -> Node nw (f' ne) sw se
-        (SW,SW) -> Node nw se (f' sw) se
-        (SE,SE) -> Node nw se sw (f' se)
-
-        (NW,NE) -> f' nw `par` f' ne `pseq` Node (f' nw) (f' ne) sw se
-        (SW,SE) -> f' sw `par` f' se `pseq` Node nw ne (f' sw) (f' se)
-        (NW,SW) -> f' nw `par` f' sw `pseq` Node (f' nw) ne (f' sw) se
-        (NE,SE) -> f' ne `par` f' se `pseq` Node nw (f' ne) sw (f' se)
-
-        _       -> f' nw `par` f' ne `par` f' sw `par` f' se `pseq` Node (f' nw) (f' ne) (f' sw) (f' se)
-    where f' = f g (as,bs)
-
-modifyQuadR :: Eq a => (Quad a -> Quad a) -> ([Direction], [Direction]) -> Quad a -> Quad a
-modifyQuadR f (as,bs) Empty    | fullcoverage as bs = f Empty
-modifyQuadR f (as,bs) (Leaf a) | fullcoverage as bs = f (Leaf a)
-modifyQuadR f (as,bs) q        = collapse . partial modifyQuadR f (as,bs) . expand $ q
-
-setQuadR :: Eq a => Quad a -> ([Direction], [Direction]) -> Quad a -> Quad a
-setQuadR v (as,bs) Empty    | fullcoverage as bs = v
-setQuadR v (as,bs) (Leaf _) | fullcoverage as bs = v
-setQuadR v (as,bs) q        = collapse . partial setQuadR v (as,bs) . expand $ q
 
 step :: (Bool, Bool) -> Direction
 step (False, False) = NW
@@ -80,10 +53,11 @@ step (True , True ) = SE
 
 posbits :: Int -> Vec2 -> [(Bool, Bool)]
 posbits 0 _     = []
-posbits h (x,y) = (testBit x h, testBit y h):posbits (h-1) (x,y)
+posbits h (x,y) = (testBit x h', testBit y h'):posbits h' (x,y)
+                  where h' = h - 1
 
 at :: Int -> Vec2 -> [Direction]
-at h pos   = map step (posbits h pos)
+at h pos = map step (posbits h pos)
 
 modify :: Eq a => (Quad a -> Quad a) -> Vec2 -> Quadtree a -> Quadtree a
 modify f pos (Quadtree h q) = Quadtree h . modifyQuad f (at h pos) $ q
@@ -100,28 +74,53 @@ delete = set Empty
 lookup :: Eq a => Vec2 -> Quadtree a -> Quad a
 lookup pos (Quadtree h q) = findQuad (at h pos) q
 
-orderPos :: (Vec2, Vec2) -> (Vec2, Vec2)
-orderPos ((a,b),(c,d)) | a <= c && b <= d = ((a,b),(c,d))
-                       | a <= c && b >  d = ((a,d),(c,b))
-                       | a >  c && b >  d = ((c,d),(a,b))
-                       | a >  c && b >  d = ((c,d),(a,b))
-
-atR :: Int -> (Vec2, Vec2) -> ([Direction], [Direction])
-atR h r = let ((a,b),(c,d)) = orderPos r
-          in  (at h (a,b), at h (c,d))
-
-modifyRange :: Eq a => (Quad a -> Quad a) -> (Vec2, Vec2) -> Quadtree a -> Quadtree a
-modifyRange f r (Quadtree h q) = Quadtree h . modifyQuadR f (atR h r) $ q
-
-setRange :: Eq a => a -> (Vec2, Vec2) -> Quadtree a -> Quadtree a
-setRange v r (Quadtree h q) = Quadtree h . setQuadR (Leaf v) (atR h r) $ q
-
-deleteRange :: Eq a => (Vec2, Vec2) -> Quadtree a -> Quadtree a
-deleteRange r (Quadtree h q) = Quadtree h . setQuadR Empty (atR h r) $ q
-
 find :: Eq a => Vec2 -> Quadtree a -> a
 find pos q =
     case Quadtree.lookup pos q of
         Leaf v  -> v
         _       -> error "find: not leaf"
 
+orderPos :: (Vec2, Vec2) -> (Vec2, Vec2)
+orderPos ((a,b),(c,d)) | a <= c && b <= d = ((a,b),(c,d))
+                       | a <= c && b >  d = ((a,d),(c,b))
+                       | a >  c && b >  d = ((c,d),(a,b))
+                       | a >  c && b >  d = ((c,d),(a,b))
+
+atR :: Int -> (Vec2, Vec2) -> [[Direction]]
+atR h r = atR' [] (at' NW (a,b), at' NE (c,b), at' SW (a,d), at' SE (c,d))
+    where   ((a,b),(c,d)) = orderPos r
+            at' rd pos = reverse $ dropWhile (==rd) $ reverse $ at h pos
+            atR' p ( [],  [],  [],  []) = [p]
+            atR' p (is', js', ks', ls') =
+                let (i,is) = match NW is'
+                    (j,js) = match NE js'
+                    (k,ks) = match SW ks'
+                    (l,ls) = match SE ls'
+                in case (i, j, k, l) of
+                        (NW,NE,SW,SE) -> atR' (NW:p) (is, [], [], []) ++
+                                         atR' (NE:p) ([], js, [], []) ++
+                                         atR' (SW:p) ([], [], ks, []) ++
+                                         atR' (SE:p) ([], [], [], ls)
+                        ( _, _,NW, _) -> atR' (NW:p) (is, [], ks, []) ++
+                                         atR' (NE:p) ([], js, [], ls)
+                        ( _, _, _,NE) -> atR' (NW:p) (is, [], ks, []) ++
+                                         atR' (NE:p) ([], js, [], ls)
+                        (SW, _, _, _) -> atR' (SW:p) (is, [], ks, []) ++
+                                         atR' (SE:p) ([], js, [], ls)
+                        ( _,SE, _, _) -> atR' (SW:p) (is, [], ks, []) ++
+                                         atR' (SE:p) ([], js, [], ls)
+                        (NE, _, _, _) -> atR' (NE:p) (is, ks, [], []) ++
+                                         atR' (SE:p) ([], [], ks, ls)
+                        ( _, _,SE, _) -> atR' (NE:p) (is, ks, [], []) ++
+                                         atR' (SE:p) ([], [], ks, ls)
+                        ( _,NW, _, _) -> atR' (NW:p) (is, ks, [], []) ++
+                                         atR' (SW:p) ([], [], ks, ls)
+                        ( _, _, _,SW) -> atR' (NW:p) (is, ks, [], []) ++
+                                         atR' (SW:p) ([], [], ks, ls)
+                        ( _, _, _,NW) -> atR' (NE:p) (is, js, ks, ls)
+                        ( _, _,NE, _) -> atR' (NE:p) (is, js, ks, ls)
+                        ( _,SW, _, _) -> atR' (SW:p) (is, js, ks, ls)
+                        (SE, _, _, _) -> atR' (SE:p) (is, js, ks, ls)
+                        _             -> error $ "bad partial" ++ show (is', js', ks', ls')
+            match md []     = (md,[])
+            match _  (x:xs) = (x,xs)
--- t.hs
+++ t.hs
@@ -7,7 +7,7 @@ main = do
         --s az (x,y,z) = insert z (x,y) az
         --c = [(x,y,x*y) | x <- [0..2047], y <- [0..2047]]
         --t' = foldl' s t c
-        t' = setRange 1 ((0,0),(0,0)) t
+        t' = setRange 1 ((0,0),(2,2)) t
         v = foldl' (+) 0 [find (x,y) t' | x <- [0..2047], y <- [0..2047]]
 
     print t'
