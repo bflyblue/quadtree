02fc614f2d3f7493d2f195da48cab16277e4831d
Shaun Sharples <shaun.sharples@gmail.com>
2013-06-23 13:52:21 +0200

    formatting

--- Quadtree.hs
+++ Quadtree.hs
@@ -140,7 +140,7 @@ atR h r = atR' (at' NW (a,b), at' NE (c,b), at' SW (a,d), at' SE (c,d))
                 | a' >  c' && b' >  d' = ((c',d'),(a',b'))
 
 modifyRange :: Eq a => (Quad a -> Quad a) -> (Vec2,Vec2) -> Quadtree a -> Quadtree a
-modifyRange f rng (Quadtree h q) = Quadtree h .  foldl (.) id (map (modifyQuad f) (atR h rng)) $ q
+modifyRange f rng (Quadtree h q) = Quadtree h . foldl (.) id (map (modifyQuad f) (atR h rng)) $ q
 
 setRange :: Eq a => Quad a -> (Vec2,Vec2) -> Quadtree a -> Quadtree a
 setRange v = modifyRange (const v)
